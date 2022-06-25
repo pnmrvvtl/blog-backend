119348724a8674047d7afe6247b2d792f716dd6a
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-06-25 22:35:46 +0300

    created basic http server with express.js

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://pnmrvvtl:qwerty00@cluster0.ty9ioi3.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error ', err))
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/auth/login/', (req,res) => {
+    console.log(req.body);
+
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: "Vasya Pupkin",
+        },
+        'secret123'
+    );
+    res.json({
+        success: true,
+        token
+    })
+})
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Server OK');
+})
